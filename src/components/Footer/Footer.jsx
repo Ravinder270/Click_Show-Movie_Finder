5197eee070125bb6bd6c4d992255d3f45860deec
Ravinder270 <107816753+Ravinder270@users.noreply.github.com>
2023-06-21 18:47:41 +0530

    Updated  Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,15 @@ const Footer = () => {
     return (
         <footer id="footer" className={styles.footer}>
             <p>
-               
+            Made by
+                <a
+                    href="https://github.com/Ravinder270"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                > Ravinder Singh </a>
+                <br/>
+                Powered by: <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer"><img height="14"src={tmdb} alt="The Movie DB (TMDB)"></img></a>
+
             </p>
         </footer>
     );
